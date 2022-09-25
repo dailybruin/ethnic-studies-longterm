7942bf2d486dec9263a4a2c7f09a0755da261670
Tracy Zhao <tracyzhao24@g.ucla.edu>
2022-09-25 14:21:53 -0700

    article and divider image

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import styled from 'styled-components';
 import './App.css';
 import Header from './components/Header';
 import Footer from './components/Footer';
+import Article from './components/Article';
 
 function App() {
   const [ data, setData ] = useState(null);
@@ -20,7 +21,7 @@ function App() {
   return data && (
     <div className="App">
       <Header/>
-      Hello Daily Bruin!
+        <Article content={data.content} />
       <Footer/>
     </div>
   );
