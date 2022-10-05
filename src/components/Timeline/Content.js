c67bda1b5995bdba288f3a9973669d04867b12df
Lindseyp314 <lindseyp314@ucla.edu>
2022-10-04 21:11:18 -0700

    tried new library for timeline

--- src/components/Timeline/Content.js
+++ src/components/Timeline/Content.js
@@ -1,40 +1,49 @@
 export const config = {
     Dates: [
         {
-            Date: "October 2020",
-            Text: "BOARS initially approves a workgroup for the ethnic studies requirement in October 2020."
+            title: "October 2020",
+            url: null,
+            cardSubtitle: "BOARS initially approves a workgroup for the ethnic studies requirement in October 2020."
         },
         {
-            Date: "November 2021",
-            Text: "BOARS approves an initial draft for the Academic Council to place under systemwide review, with some concerns."
+            title: "November 2021",
+            url: null,
+            cardSubtitle: "BOARS approves an initial draft for the Academic Council to place under systemwide review, with some concerns."
         },
         {
-            Date: "March 2022",
-            Text: "Systemwide review generates largely supportive feedback from every campus, with notable concerns, and the Academic Council decides to send the proposal back to BOARS."
+            title: "March 2022",
+            url: null,
+            cardSubtitle: "Systemwide review generates largely supportive feedback from every campus, with notable concerns, and the Academic Council decides to send the proposal back to BOARS."
         },
         {
-            Date: "April 2022",
-            Text: "Ethnic Studies workgroup addresses critiques and provides revisions."
+            title: "April 2022",
+            url: null,
+            cardSubtitle: "Ethnic Studies workgroup addresses critiques and provides revisions."
         },
         {
-            Date: "May 2022",
-            Text: "In a May meeting, BOARS remains unswayed."
+            title: "May 2022",
+            url: null,
+            cardSubtitle: "In a May meeting, BOARS remains unswayed."
         },
         {
-            Date: "Later May 2022",
-            Text: "Christine Hong and Andrew Jolivette issue a May statement defending the requirement as written."
+            title: "Later May 2022",
+            url: null,
+            cardSubtitle: "Christine Hong and Andrew Jolivette issue a May statement defending the requirement as written."
         },
         {
-            Date: "May 31, 2022",
-            Text: "A letter to BOARS from some UC faculty sharply criticizes the requirement."
+            title: "May 31, 2022",
+            url: null,
+            cardSubtitle: "A letter to BOARS from some UC faculty sharply criticizes the requirement."
         },
         {
-            Date: "June 2022",
-            Text: "In the BOARS’ June meeting, several local admissions committees are split on whether to continue or end deliberations on the requirement."
+            title: "June 2022",
+            url: null,
+            cardSubtitle: "In the BOARS’ June meeting, several local admissions committees are split on whether to continue or end deliberations on the requirement."
         },
         {
-            Date: "July 2022",
-            Text: "In the BOARS’ July meeting, a possible working group tackling concerns about the implementation of the requirement is suggested."
+            title: "July 2022",
+            url: null,
+            cardSubtitle: "In the BOARS’ July meeting, a possible working group tackling concerns about the implementation of the requirement is suggested."
         }
     ]
 };
